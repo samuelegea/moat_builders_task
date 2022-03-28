d7ea46a447d39a62053c7a8f87b6cfcdb3bcd5fb
Samuel Egea <samuelegea@hotmail.com>
2022-03-28 20:35:04 -0300

    feat: formatted login and signup forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, authentication_keys: [:username]
+  
+  validates :username, :fullname, presence: true
+  validates :username, uniqueness: true
 
   
 
